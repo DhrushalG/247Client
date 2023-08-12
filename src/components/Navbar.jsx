79535ee66b1b2a4219f04751e415ea33b830e580
DhrushalG <dhrushalgada91@gmail.com>
2023-08-12 16:31:07 -0500

    Sizing2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,25 +51,25 @@ const Navbar = () => {
 
             {/* SMALL */}
             <Box sx={{ justifyContent: 'between', flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <div className='navbar-nav d-flex justify-content-between' style={{ width: '100%' }}>
-                    <div className=' d-flex align-items-center' style={{ minWidth: '40%' }}>
+                <div className='navbar-nav d-flex' style={{ width: '100%' }}>
+                    <div className=' d-flex align-items-center' style={{ minWidth: '57%' }}>
                         <Link className='nav-link' to='/'>
-                            <img src={Logo} className='p-1' alt='' style={{ backgroundColor: 'white', boxShadow: '50px -165px 40px 160px rgba(30, 158, 130, 1)', borderRadius: '10px' }} />
+                            <img src={Logo} className='' alt='' style={{ backgroundColor: 'white', boxShadow: '50px -165px 40px 160px rgba(30, 158, 130, 1)', borderRadius: '10px' }} />
                         </Link>
-                        <Link class=' mt-2 nav-link h5' style={{ width: '150px' }} to='/'>24/7 Assure Care
+                        <Link class=' mt-2 nav-link h5' style={{}} to='/'>24/7 Assure Care
                         </Link>
 
                     </div>
 
-                    <div className='d-flex align-items-center'>
-                        <div>
-                            <Link class='mt-2 nav-link h6' style={{ width: '115px', height: '100%' }} to='/'> * 847-483-8999
+                    <div className='d-flex justify-content-end align-items-center' style={{ width: '50%' }}>
+                        <div className='' style={{ width: '80%' }}>
+                            <Link class='mt-2 nav-link h6' style={{ width: '110px', height: '100%' }} to='/'> * 847-483-8999
                             </Link>
                         </div>
 
                         <div class=''>
                             <MenuItem aria-controls="menu-appbar" onClick={handleOpenNavMenu} color='#1B8C79' sx={{ borderRadius: '10px' }}>
-                                <MenuIcon style={{ height: '30px', width: '30px', color: 'white', border: 'none' }} />
+                                <MenuIcon style={{ height: '20px', width: '20px', color: 'white', border: 'none' }} />
                             </MenuItem>
                             <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} keepMounted
                                 transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu}

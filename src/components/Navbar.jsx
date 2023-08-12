a2de39e17bc929fbb7280f89dc45ee610ff314d3
DhrushalG <dhrushalgada91@gmail.com>
2023-08-12 16:57:14 -0500

    looping

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,13 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 import Logo from '../images/logo.jpg';
 
+const navLinks = [
+    { to: '/AboutUs', text: 'About Us' },
+    { to: '/Services', text: 'Services' },
+    { to: '/Contact', text: 'Contact Us' },
+    { to: '/Resources', text: 'Resources' }
+];
+
 const Navbar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
 
@@ -22,9 +29,7 @@ const Navbar = () => {
         <nav class='navbar-fixed navbar-dark navbar-expand' style={{ backgroundColor: '#1B8C79' }}>
             {/* BIG */}
             <Box sx={{ px: 1, justifyContent: 'center', flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-
                 <div class='navbar-nav align-items-center d-flex justify-content-between' style={{ width: '100%' }}>
-
                     <div className=' d-flex align-items-center' style={{ minWidth: '25%' }}>
                         <Link className='nav-link' to='/'>
                             <img src={Logo} className='p-1' alt='' style={{ backgroundColor: 'white', boxShadow: '50px -165px 40px 160px rgba(30, 158, 130, 1)', borderRadius: '10px' }} />
@@ -33,20 +38,17 @@ const Navbar = () => {
                             24/7 Assure Care
                         </Link>
                     </div>
-
                     <div class='d-flex flex-wrap justify-content-center ' style={{ minWidth: '50%' }}>
-                        <Link class='nav-link mx-2' to='/AboutUs'>About Us</Link>
-                        <Link class='nav-link mx-2 ' to='/Services'>Services</Link>
-                        <Link class='nav-link mx-2 ' to='/Contact'>Contact Us</Link>
-                        <Link class='nav-link mx-2' to='/Resources'>Resources</Link>
+                        {navLinks.map((link, index) => (
+                            <Link key={index} className='nav-link mx-2' to={link.to}>
+                                {link.text}
+                            </Link>
+                        ))}
                     </div>
-
                     <div className='d-flex justify-content-end px-3' style={{ minWidth: '25%' }}>
                         <Link className='nav-link' to='/'>847-483-8999</Link>
                     </div>
-
                 </div>
-
             </Box>
 
             {/* SMALL */}
@@ -58,15 +60,12 @@ const Navbar = () => {
                         </Link>
                         <Link class=' mt-2 nav-link h5' style={{}} to='/'>24/7 Assure Care
                         </Link>
-
                     </div>
-
                     <div className='d-flex justify-content-end align-items-center' style={{ width: '50%' }}>
                         <div className='' style={{ width: '115px' }}>
                             <Link class='mt-2 nav-link h6' style={{ width: '100%', height: '100%' }} to='/'> * 847-483-8999
                             </Link>
                         </div>
-
                         <div class=''>
                             <MenuItem aria-controls="menu-appbar" onClick={handleOpenNavMenu} color='#1B8C79' sx={{ borderRadius: '10px' }}>
                                 <MenuIcon style={{ height: '25px', width: '25px', color: 'white', border: 'none' }} />
@@ -74,28 +73,17 @@ const Navbar = () => {
                             <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} keepMounted
                                 transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu}
                                 PaperProps={{ style: { background: '#1B8C79' } }} sx={{ display: { xs: 'block', md: 'none' } }}>
-                                <div className=''>
-                                    <MenuItem>
-                                        <Link onClick={handleCloseNavMenu} class='h4' style={{ color: 'white', textDecoration: 'none' }} to='/AboutUs'>About Us</Link>
+                                {navLinks.map((link, index) => (
+                                    <MenuItem key={index}>
+                                        <Link onClick={handleCloseNavMenu} className="h4" style={{ color: 'white', textDecoration: 'none' }} to={link.to}>
+                                            {link.text}
+                                        </Link>
                                     </MenuItem>
-                                    <MenuItem>
-                                        <Link onClick={handleCloseNavMenu} class='h4' style={{ color: 'white', textDecoration: 'none' }} to='/Services'>Services</Link>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <Link onClick={handleCloseNavMenu} class='h4' style={{ color: 'white', textDecoration: 'none' }} to='/Contact'>Contact Us</Link>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <Link onClick={handleCloseNavMenu} class='h4' style={{ color: 'white', textDecoration: 'none' }} to='/Resources'>Resources</Link>
-                                    </MenuItem>
-                                </div>
+                                ))}
                             </Menu>
                         </div>
-
                     </div>
                 </div>
-
-
-
             </Box>
         </nav>
     )
